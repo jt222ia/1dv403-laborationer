ffc3cdf6c862adbad0f4a6f9179ca232e51c0f1e
jt222ia <jt222ia@student.lnu.se>
2014-11-04 16:49:19 +0100

    String gick bra men det ett tag

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -3,16 +3,43 @@
 window.onload = function(){
 
 	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var convertString = function(str){
-		// Plats för förändring.		
-		// Returnera den konverterade strängen.
-		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
 	
+	
+	var convertString = function(str){
+	var strLength = str.length;
+    var k;
+    var stringArray = [];
+	 
 
+        if (str === "")
+		{ 
+			// Undantag kastas!
+            throw new Error("Du måste skriva in text i fältet, tack!");
+        }
+        
+        for (k = 0; k < strLength; k += 1) {
+            
+            stringArray[k] = str[k]; // sätter in text-strängen i en array
+            
+            if (stringArray[k] === "a" || stringArray[k] === "A") 
+			{ 
+				// Omvandlar a och A till #
+                stringArray[k] = "#";
+                
+            } else if (stringArray[k] === stringArray[k].toUpperCase())
+			{ 
+				// omvandlar UpperCase-bokstäver till LowerCase
+               stringArray[k] = stringArray[k].toLowerCase();
 
-
-
-
+            } else if (stringArray[k] === stringArray[k].toLowerCase()) { // omvandlar LowerCase-bokstäver till UpperCase
+                
+                stringArray[k] = stringArray[k].toUpperCase();
+            }
+        }
+        return stringArray.join(""); // returnerar en sträng utav arrayen
+	// Plats för förändring.		
+	// Returnera den konverterade strängen.
+	// Vid fel, kasta ett undantag med ett meddelande till användaren. 
 
 	};
 	// ------------------------------------------------------------------------------
